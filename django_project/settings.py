afcaa666df48d2d1cd4542dd87641a9cd415955a
aryankaRasetty <165583086+AryankaR-hub@users.noreply.github.com>
2026-04-05 11:19:31 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 DEBUG = env.bool("DEBUG", default=False)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-DEBUG =     True
+DEBUG =     False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
